0375d613e9b6477de183465cd3e949520eec74fc
Artamta <artamta47@gmail.com>
2025-03-19 14:59:46 +0530

    Create binary_tree_treversal_20250319014902.cpp

---
+++ .history/Assignments/Assignment5/binary_tree_treversal_20250319014902.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(struct Node *root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " " << endl;
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    preorder(root->left);
+    cout << root->data << " " << endl;
+    preorder(root->right);
+}
+
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    root->right->left = new Node(7);
+    // preorder(root);
+    // inorder(root);
+    postorder(root);
+    return 0;
+}
